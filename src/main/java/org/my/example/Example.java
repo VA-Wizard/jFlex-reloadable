6e1bbf67a64705f90c7135c12601c8e772499f8a
va <va.wizard@gmail.com>
2019-11-05 10:47:38 +0300

    Refactor

---
+++ src/main/java/org/my/example/Example.java
@@ -0,0 +1,25 @@
+package org.my.example;
+
+import org.my.factory.FlexFactory;
+import org.my.handler.FlexHandler;
+
+import java.io.*;
+
+public class Example {
+
+    public static void main(String[] args) throws Exception {
+        Example.class.getClassLoader().getResourceAsStream("example/example_numbers_0_9.jflex");
+        try(Reader reader = new InputStreamReader(Example.class.getClassLoader().getResourceAsStream("example/example_numbers_0_9.jflex"))){
+            FlexHandler handler = FlexFactory.newFlexHandler(reader, new StringReader("0123456789"));
+            handler.yylex();
+            System.err.println(handler.yytext());
+        }
+
+        try(Reader reader = new InputStreamReader(Example.class.getClassLoader().getResourceAsStream("example/example_numbers_0_5.jflex"))){
+            FlexHandler handler = FlexFactory.newFlexHandler(reader, new StringReader("0123456789"));
+            handler.yylex();
+            System.err.println(handler.yytext());
+        }
+
+    }
+}
